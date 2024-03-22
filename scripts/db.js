b18d15c2e4b36b1b8df88c6543fe7c6d18c5a9d0
NelsonCC <nelsoncc1812@gmail.com>
2024-03-22 01:58:50 +0000

    working on form functionality

--- scripts/db.js
+++ scripts/db.js
@@ -1,4 +1,17 @@
-const sendForm = e => {
+const sendData = e => {
     e.preventDefault()
 
+    const db = firebase.initializeApp(FIRE_KEY).firestore()
+
+    const name = document.querySelector('#name')
+    const companion = document.querySelector('#companion')
+    const withKids = document.querySelector('#withKids')
+    const others = document.querySelector('#others')
+
+    db.collection('Items').add({ name, companion, withKids, others })
+    then(elm => {
+        if (elm == null) return document.querySelector('#form-error').classList.remove('hidden')
+        return document.querySelector('#form-correct').classList.remove('hidden')
+    })
+        .catch(err => document.querySelector('#form-error').classList.remove('hidden'))
 }
