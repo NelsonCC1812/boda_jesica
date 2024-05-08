b174e6caf03d33bd5a05aebe703aae8cb09d8f77
NelsonCC1812 <nelsoncc1812@gmail.com>
2024-05-08 14:41:40 +0100

    Add files via upload

---
+++ firebase.key.js
@@ -0,0 +1,8 @@
+const FIRE_KEY = {
+  "apiKey": "AIzaSyAt7bBkIVdfBZ-pkcG4JbgJmNVqG5FAObk",
+  "authDomain": "wedding-e97b5.firebaseapp.com",
+  "projectId": "wedding-e97b5",
+  "storageBucket": "wedding-e97b5.appspot.com",
+  "messagingSenderId": "200025207139",
+  "appId": "1:200025207139:web:06fafc00de39a9bad4e356"
+}
